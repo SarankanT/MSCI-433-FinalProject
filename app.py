631e8d0047f501ced2771245eb5c8a03c4ecf467
Sarankan Thiru <sarankan.thiru@yahoo.ca>
2024-02-27 23:34:44 -0500

    pushing for hassan

---
+++ app.py
@@ -0,0 +1,13 @@
+# Cheatsheet available at https://docs.streamlit.io/library/cheatsheet
+import streamlit as st
+import pandas as pd
+from pandas import read_csv
+
+
+st.set_page_config(layout="wide")
+
+st.title('Welcome to Kings Landscaping Solutions!')
+st.write("By: Hassan Rawasia, Sarankan Thirunavukkarasu, Skyler Nam-Wong")
+
+st.write("With our extensive experience and dedication to excellence, we specialize in transforming houses into dream homes worthy of a KING! :crown:")
+st.write("Whether you need a kitchen remodel, bathroom upgrade, complete home renovation, or stylish additions, our skilled team of professionals is committed to bringing your vision to life. We provide personalized design solutions, transparent communication, and exceptional customer service to ensure a seamless renovation process. Let us turn your house into the home of your dreams - contact us today to schedule a consultation and experience the highest quality home renovations in Ames, Iowa.")
